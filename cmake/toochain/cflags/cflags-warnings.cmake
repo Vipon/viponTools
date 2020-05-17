5605f351c68cd0986e071b3212729d01b5de4374
Vipon <valerakonychev@gmail.com>
2020-05-17 19:53:23 +0300

    Add warning flags.

---
+++ cmake/toochain/cflags/cflags-warnings.cmake
@@ -0,0 +1,25 @@
+include(${CMAKE_CURRENT_LIST_DIR}/cflags.cmake)
+
+set(WARNING_FLAGS)
+append_cflagc(WARNING_FLAGS "-Wall -Wextra -Wcast-qual -Wnewline-eof -Wparentheses -Wsign-conversion -fdiagnostics-color=always")
+
+if("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
+  # clang doesn't support these flags (this causes too many warnings)
+  append_cflagc(WARNING_FLAGS "-Wduplicated-branches -Wduplicated-cond")
+endif()
+
+set(WARNING_CXXFLAGS "${WARNING_FLAGS}")
+
+# only C warning flags
+set(WARNING_CFLAGS)
+append_cflagc(WARNING_CFLAGS "-Wmissing-prototypes -Wstrict-prototypes")
+
+# only CXX warning flags
+set(WARNING_CXXFLAGS)
+
+string(APPEND C_FLAGS   " ${WARNING_FLAGS}")
+string(APPEND C_FLAGS   " ${WARNING_CFLAGS}")
+
+string(APPEND CXX_FLAGS " ${WARNING_FLAGS}")
+string(APPEND CXX_FLAGS " ${WARNING_CXXFLAGS}")
+
