f364578c8c0398f292c5db9b328c12c09ddb7266
Vipon <valerakonychev@gmail.com>
2020-05-18 15:19:32 +0300

    Add debug flags.

--- cmake/toochain/cflags/cflags-coverage.cmake
+++ cmake/toochain/cflags/cflags-coverage.cmake
@@ -3,16 +3,16 @@ include(${CMAKE_CURRENT_LIST_DIR}/cflags.cmake)
 set(COVERAGE_CFLAGS)
 
 # Need to specify libs for link, because cmake checking with linking.
-set(CMAKE_REQUIRED_LIBRARIES "--coverage")
+set(CMAKE_REQUIRED_LIBRARIES --coverage)
 if("${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
-  append_cflag(COVERAGE_CFLAGS "--coverage -fprofile-abs-path")
+  append_cflag(COVERAGE_CFLAGS --coverage -fprofile-abs-path)
 elseif("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang")
-  append_cflag(COVERAGE_CFLAGS "-fprofile-instr-generate -fcoverage-mapping")
+  append_cflag(COVERAGE_CFLAGS -fprofile-instr-generate -fcoverage-mapping)
 endif()
 
-append_cflag(COVERAGE_CFLAGS "-fno-omit-frame-pointer")
+append_cflag(COVERAGE_CFLAGS -fno-omit-frame-pointer)
 unset(CMAKE_REQUIRED_LIBRARIES)
 
-string(APPEND C_FLAGS_DEBUG   " ${COVERAGE_CFLAGS}")
-string(APPEND CXX_FLAGS_DEBUG " ${COVERAGE_CFLAGS}")
+string(APPEND C_FLAGS_DEBUG   "${COVERAGE_CFLAGS}")
+string(APPEND CXX_FLAGS_DEBUG "${COVERAGE_CFLAGS}")
 
