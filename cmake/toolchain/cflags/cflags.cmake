f135f3732c5b700ff4176d4a584c238ba29c43d3
Vipon <valerakonychev@gmail.com>
2020-05-20 11:19:42 +0300

    Update cflags.
    Add common cflags and fix adding C_FLAGS to building.

--- cmake/toolchain/cflags/cflags.cmake
+++ cmake/toolchain/cflags/cflags.cmake
@@ -12,9 +12,18 @@ macro(append_cflags var)
 endmacro()
 
 #enable_language(C)
+include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/debug.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/release.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/coverage.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/sanitizers.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/warnings.cmake)
+
+set(CMAKE_C_FLAGS ${C_FLAGS})
+set(CMAKE_C_FLAGS_DEBUG ${C_FLAGS_DEBUG})
+set(CMAKE_C_FLAGS_RELEASE ${C_FLAGS_RELEASE})
+
+set(CMAKE_CXX_FLAGS ${CXX_FLAGS})
+set(CMAKE_CXX_FLAGS_DEBUG ${CXX_FLAGS_DEBUG})
+set(CMAKE_CXX_FLAGS_RELEASE ${CXX_FLAGS_RELEASE})
 
