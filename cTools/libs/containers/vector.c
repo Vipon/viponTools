9f386f0bee6f8eebddd8a551a4daeed39d1a6c7c
Vipon <valerakonychev@gmail.com>
2020-05-25 14:01:07 +0300

    Add new functions and test for vector.

--- cTools/libs/containers/vector.c
+++ cTools/libs/containers/vector.c
@@ -1,11 +1,13 @@
 #include "mem.h"
+#include "bits.h"
 #include "vector.h"
 #include "comdef.h"
 
 #include <stddef.h>
 
+#define GET_PTR_VECTOR_ELEM(v, num) (v->data + num * v->elemSize)
 
-int vectorInit(Vector *v, size_t capacity, size_t elemSize)
+int initVector(Vector *v, size_t capacity, size_t elemSize)
 {
     if ((v->data = Malloc(capacity * elemSize)) == NULL) {
         ERROR("Cannot allocate memory");
@@ -20,7 +22,7 @@ int vectorInit(Vector *v, size_t capacity, size_t elemSize)
 }
 
 
-void vectorFree(Vector *v)
+void freeVector(Vector *v)
 {
     Free(v->data);
     v->capacity = (size_t)-1;
@@ -29,7 +31,42 @@ void vectorFree(Vector *v)
 }
 
 
-static int vectorExpand(Vector *v)
+uint8_t *beginVector(Vector *v)
+{
+    return v->data;
+}
+
+
+uint8_t *endVector(Vector *v)
+{
+    return GET_PTR_VECTOR_ELEM(v, v->end);
+}
+
+
+int resizeVector(Vector *v, size_t capacity)
+{
+    v->capacity = capacity;
+    void *data = Malloc(capacity * v->elemSize);
+    if (data == NULL) {
+        ERROR("Cannot allocate memory");
+        return -1;
+    }
+
+    if (v->end > capacity) {
+        v->end = capacity;
+    }
+
+    size_t num = v->end;
+    directCopyBytes(v->data, data, num * v->elemSize);
+
+    Free(v->data);
+    v->data = data;
+
+    return 0;
+}
+
+
+static int expandVector(Vector *v)
 {
     v->capacity *= 2;
     void *data = Malloc(v->capacity * v->elemSize);
@@ -46,42 +83,59 @@ static int vectorExpand(Vector *v)
 }
 
 
-int vectorPushBack(Vector *v, const void *elem)
+int pushBackVector(Vector *v, const void *elem)
 {
     if (v->capacity == v->end) {
-        if (vectorExpand(v) == -1) {
+        if (expandVector(v) == -1) {
             ERROR("Cannot expand Vector");
             return -1;
         }
     }
 
-    directCopyBytes(elem, v->data + (v->end * v->elemSize), v->elemSize);
+    directCopyBytes(elem, endVector(v), v->elemSize);
     ++v->end;
 
     return 0;
 }
 
 
-void *vectorPopBack(Vector *v)
+void *popBackVector(Vector *v)
 {
     if (v->end == 0) {
         ERROR("Vector is empty");
         return NULL;
     }
 
-    void *elem = v->data + (v->end * v->elemSize);
     --v->end;
-    return elem;
+    return endVector(v);
 }
 
 
-void *vectorGet(Vector *v, size_t num)
+int setElemVector(Vector *v, size_t num, const void *elem)
+{
+    if (num >= v->capacity) {
+        if (resizeVector(v, ROUND_UP_2(num))) {
+            ERROR("Cannot resize Vector");
+            return -1;
+        }
+    }
+
+    directCopyBytes(elem, v->data + (num * v->elemSize), v->elemSize);
+
+    if (num > v->end) {
+        v->end = num + 1;
+    }
+
+    return 0;
+}
+
+
+void *getElemVector(Vector *v, size_t num)
 {
     if (num >= v->end) {
         return NULL;
     }
 
-    return v->data + (num * v->elemSize);
+    return GET_PTR_VECTOR_ELEM(v, num);
 }
-
 
