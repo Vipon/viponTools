b4f2ff52ca5b1969a82d35d10b99c7915364908c
Vipon <valerakonychev@gmail.com>
2020-05-26 16:56:03 +0300

    Add generig forEach.

--- cTools/libs/containers/containers.h
+++ cTools/libs/containers/containers.h
@@ -4,5 +4,12 @@
 #include "vector.h"
 #include <stdint.h>
 
+#if __STDC_VERSION__ >= 201112L
+# define forEachContainer(c, func, ...) \
+    _Generic((c),                       \
+        Vector : forEachVector          \
+    )(c, func, __VA_ARGS__)
+#endif
+
 #endif /* __CONTEINERS_H  */
 
