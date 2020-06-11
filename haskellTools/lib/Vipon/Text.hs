65f17943cd118ae6b2c05e7723e48629b771b131
Vipon <valerakonychev@gmail.com>
2020-06-11 19:09:32 +0300

    Improve palindrome function.

--- haskellTools/lib/Vipon/Text.hs
+++ haskellTools/lib/Vipon/Text.hs
@@ -1,7 +1,26 @@
 module Vipon.Text
-  ( isPalindrome
+  ( stripWhiteSpace
+  , stripPunctuation
+  , toLower
+  , palindromePreprocess
+  , isPalindrome
   ) where
 
+import qualified Data.Char as Char(toLower, isSpace, isPunctuation)
+
+stripWhiteSpace :: String -> String
+stripWhiteSpace = filter (not . Char.isSpace)
+
+stripPunctuation :: String -> String
+stripPunctuation = filter (not . Char.isPunctuation)
+
+toLower :: String -> String
+toLower = map Char.toLower
+
+palindromePreprocess :: String -> String
+palindromePreprocess = stripWhiteSpace . stripPunctuation . toLower
+
 isPalindrome :: String -> Bool
-isPalindrome t = t == reverse t
+isPalindrome t = cleanText == reverse cleanText
+  where cleanText = palindromePreprocess t
 
