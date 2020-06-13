cecf898c2f9dbacdf94f16481467e1e089656333
Vipon <valerakonychev@gmail.com>
2020-06-13 22:00:40 +0300

    Init tool directory.
    Directory will contain different executable tools.

---
+++ haskellTools/tool/isPalindrome/app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Vipon.Text
+
+main :: IO ()
+main = do
+  print "Enter a word and I'll let you know if it's a palindrome!"
+  text <- getLine
+  let response = if isPalindrome text
+                  then "it is!"
+                  else "it's not!"
+  print response
