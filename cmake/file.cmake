5ec225f31b78866e0752ccb541ccb928dd0567e4
Vipon <valerakonychev@gmail.com>
2020-06-18 17:34:13 +0300

    Add new functions to file.cmake lib.
    removeDir - remove directory
    extractArchive - extracting arhive using tar.

--- cmake/file.cmake
+++ cmake/file.cmake
@@ -1,10 +1,19 @@
 include_guard()
 
+
 function(createDir DIR_PATH)
   if(NOT EXISTS "${DIR_PATH}")
+    message(STATUS "Create directory ${DIR_PATH}")
     file(MAKE_DIRECTORY "${DIR_PATH}")
   endif()
 endfunction(createDir)
+
+
+function(removeDir DIR_PATH)
+  message(STATUS "Remove directory ${DIR_PATH}")
+  file(REMOVE_RECURSE "${DIR_PATH}")
+endfunction(removeDir)
+
 
 function(downloadFile)
   cmake_parse_arguments(ARG
@@ -34,7 +43,7 @@ function(downloadFile)
   endif()
 
   get_filename_component(JUST_NAME "${ARG_FILE_NAME}" NAME)
-  set(FILE_STAMP "${FILE_DIR}/.stam_${JUST_NAME}")
+  set(FILE_STAMP "${FILE_DIR}/.download_stamp_${JUST_NAME}")
   if(ARG_CHECK_STAMP)
     if (EXISTS "${FILE_STAMP}")
       return()
@@ -53,4 +62,58 @@ function(downloadFile)
     file(TOUCH "${FILE_STAMP}")
   endif()
 endfunction(downloadFile)
+
+
+function(extractArchive)
+  cmake_parse_arguments(ARG
+    # true_false_options
+    "CHECK_STAMP"
+    # one_value_options
+    "ARH_NAME;OUT_DIR;MEMBER;STRIP_COMPONENT"
+    # multi_value_options
+    ""
+    ${ARGN}
+  )
+
+  set(TAR_ARGS xvf)
+
+  if(NOT ARG_ARH_NAME)
+    message(FATAL_ERROR "Please specify ARH_NAME")
+  endif()
+  list(APPEND TAR_ARGS "${ARG_ARH_NAME}" "${ARG_MEMBER}")
+
+  get_filename_component(ARH_DIR "${ARG_ARH_NAME}" DIRECTORY ABSOLUTE)
+
+  get_filename_component(JUST_ARH_NAME "${ARG_ARH_NAME}" NAME)
+  set(ARH_STAMP "${ARH_DIR}/.arh_stamp_${JUST_ARH_NAME}")
+  if(ARG_CHECK_STAMP)
+    if (EXISTS "${ARH_STAMP}")
+      return()
+    endif()
+  endif()
+
+  if(ARG_STRIP_COMPONENT)
+    list(APPEND TAR_ARGS "--strip-components=${ARG_STRIP_COMPONENT}")
+  endif()
+
+  if(ARG_OUT_DIR)
+    list(APPEND TAR_ARGS "--one-top-level=${ARG_OUT_DIR}")
+  endif()
+
+  message(STATUS "Extract ${ARG_ARH_NAME} to ${ARG_OUT_DIR}")
+
+  execute_process(
+    COMMAND tar ${TAR_ARGS}
+    WORKING_DIRECTORY "${ARH_DIR}"
+    RESULT_VARIABLE ret
+  )
+
+  if(NOT ${ret} EQUAL "0")
+    message(FATAL_ERROR "FAILED to extract archive ${ARG_ARH_NAME}")
+  endif()
+
+  if(ARG_CHECK_STAMP)
+    file(TOUCH "${ARH_STAMP}")
+  endif()
+endfunction(extractArchive)
 
