02afbd7ed566117bd86c6198f234520bd385062b
Vipon <valerakonychev@gmail.com>
2020-06-24 15:03:42 +0300

    Create tools for save and install packages.
    Currently only for Ubuntu.
     * os directory will contains os specific functions
     * packages directory contains libraries for simplifying of maintain
       packages.

---
+++ shellTools/os/libosRelease.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+getOsReleaseFile()
+{
+    if [ -f "/etc/os-release" ]; then
+        echo "/etc/os-release"
+    elif [ -f "/usr/lib/os-release" ]; then
+        echo "/usr/lib/os-release"
+    fi
+
+    echo ""
+}
+
+OS_RELEASE="$(getOsReleaseFile)"
+
+getOsNameFromOsRelease()
+{
+    cat "${OS_RELEASE}" | grep -oP "^NAME=\"\K(\S[^\"]*)"
+}
+
+getVersionFromOsRelease()
+{
+    cat "${OS_RELEASE}" | grep -oP "^VERSION_ID=\"\K(\S[^\"]*)"
+}
+
