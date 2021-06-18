b7b2046d13e19dcec9a004dd424f94f1caec0757
Vipon <valerakonychev@gmail.com>
2021-06-18 22:17:40 +0300

    Add others functions to binParse.

--- cTools/libs/binParse/binParse.c
+++ cTools/libs/binParse/binParse.c
@@ -4,15 +4,40 @@
 
 BinParser binParser;
 
-#define INIT_BIN_PARSER_FUNCS(type)                \
-    binParser.binParse = (BinParse)&type ## Parse; \
-    binParser.binFree = (BinFree)&type ## Free;    \
-    binParser.binHook = (BinHook)&type ## Hook;
+#define INIT_BIN_PARSER_FUNCS(type) \
+    binParser.parse = (BinParse)&type ## Parse; \
+    binParser.free = (BinFree)&type ## Free; \
+    binParser.hook = (BinHook)&type ## Hook; \
+    binParser.check = (BinFullCheck) &type ## Check; \
+    binParser.printSymbol = (BinPrintSymbol) &type ## PrintSymbol; \
+    binParser.printSymbols = (BinPrintSymbols) &type ## PrintSymbols; \
+    binParser.getSymByName = (BinGetSymByName) &type ## GetSymByName; \
+    binParser.getSymName = (BinGetSymName) &type ## GetSymName; \
+    binParser.cmpSym = (BinCmpSym) &type ## CmpSym; \
+    binParser.getSSymTab = (BinGetSSymTab) &type ## GetSSymTab; \
+    binParser.getSSymSortTab = (BinGetSSymSortTab) &type ## GetSSymSortTab; \
+    binParser.getAmountSSym = (BinGetAmountSSym) &type ## GetAmountSSym; \
+    binParser.getSSymAddr = (BinGetSSymAddr) &type ## GetSSymAddr; \
+    binParser.getAddrSymByName = (BinGetAddrSymByName) &type ## GetAddrSymByName; \
+    binParser.getSSymSize = (BinGetSSymSize) &type ## GetSSymSize; \
+    binParser.getSSymFileoff = (BinGetSSymFileoff) &type ## GetSSymFileoff; \
+    binParser.getDSymIndex = (BinGetDSymIndex) &type ## GetDSymIndex; \
+    binParser.getAmountSegment = (BinGetAmountSegment) &type ## GetAmountSegment; \
+    binParser.getSectByName = (BinGetSectByName) &type ## GetSectByName; \
+    binParser.getLastLoadableSect = (BinGetLastLoadableSect) &type ## GetLastLoadableSect; \
+    binParser.readSect = (BinReadSect) &type ## ReadSect; \
+    binParser.getAmountSect = (BinGetAmountSect) &type ## GetAmountSect; \
+    binParser.getSectName = (BinGetSectName) &type ## GetSectName; \
+    binParser.getSectSize = (BinGetSectSize) &type ## GetSectSize; \
+    binParser.getSectVaddr = (BinGetSectVaddr) &type ## GetSectVaddr; \
+    binParser.getSectFileoff = (BinGetSectFileoff) &type ## GetSectFileoff; \
+    binParser.getRelocationForAddr = (BinGetRelocationForAddr) &type ## GetRelocationForAddr; \
+    binParser.getRelocationDataAddr = (BinGetRelocationDataAddr) &type ## GetRelocationDataAddr;
 
 int initBinParser(const char *fn)
 {
-    void *binFile = NULL;
-    if ((binFile = elf64Parse(fn)) != NULL) {
+    BinFilePtr bin = NULL;
+    if ((bin = elf64Parse(fn)) != NULL) {
         binParser.type = ELF64;
         INIT_BIN_PARSER_FUNCS(elf64);
     } else {
@@ -20,19 +45,19 @@ int initBinParser(const char *fn)
         return -1;
     }
 
-    binParser.binFile = binFile;
+    binParser.bin = bin;
     return 0;
 }
 
 
 void finiBinParser()
 {
-    binParser.binFree(binParser.binFile);
+    binParser.free(binParser.bin);
 }
 
 
 void *binHook(const char *func, const void *hand)
 {
-    return binParser.binHook(binParser.binFile, func, hand);
+    return binParser.hook(binParser.bin, func, hand);
 }
 
--- cTools/libs/binParse/binParse.h
+++ cTools/libs/binParse/binParse.h
@@ -25,6 +25,8 @@
 #ifndef _BIN_PARSE_H
 #define _BIN_PARSE_H
 
+#include <elf.h>
+
 typedef enum {
     ELF64 = 0,
     MACHO64,
@@ -34,17 +36,73 @@ typedef enum {
     PE32
 } BIN_FILE_TYPE;
 
-typedef void* BinFile;
-typedef void*(*BinParse)(const char *fn);
-typedef void(*BinFree)(void *binFile);
-typedef void*(*BinHook)(const void* binFile, const char *func, const void *hand);
+// !TODO: shoud be enum
+typedef uint64_t BIN_ERROR;
+
+typedef void* BinFilePtr;
+typedef void* BinSymPtr;
+typedef void* BinSectPtr;
+
+typedef BinFilePtr (*BinParse)(const char*);
+typedef void (*BinFree)(void*);
+typedef void* (*BinHook)(const BinFilePtr, const char*, const void*);
+typedef BIN_ERROR (*BinFullCheck)(const BinFilePtr);
+typedef BIN_ERROR (*BinPrintSymbol)(const BinFilePtr, const BinSymPtr);
+typedef BIN_ERROR (*BinPrintSymbols)(const BinFilePtr);
+typedef BinSymPtr (*BinGetSymByName)(const BinFilePtr, const char*);
+typedef char* (*BinGetSymName)(const BinFilePtr, const BinSymPtr);
+typedef int (*BinCmpSym)(const void*, const void*);
+typedef BinSymPtr (*BinGetSSymTab)(const BinFilePtr);
+typedef BinSymPtr (*BinGetSSymSortTab)(const BinFilePtr);
+typedef uint64_t (*BinGetAmountSSym)(const BinFilePtr);
+typedef uint64_t (*BinGetSSymAddr)(const BinSymPtr);
+typedef uint64_t (*BinGetAddrSymByName)(const BinFilePtr, const char*);
+typedef uint64_t (*BinGetSSymSize)(const BinFilePtr, const BinSymPtr);
+typedef uint64_t (*BinGetSSymFileoff)(const BinFilePtr, const BinSymPtr);
+typedef uint64_t (*BinGetDSymIndex)(const BinFilePtr, const char*);
+typedef uint64_t (*BinGetAmountSegment)(const BinFilePtr);
+typedef BinSectPtr* (*BinGetSectByName)(const BinFilePtr, const char*);
+typedef BinSectPtr* (*BinGetLastLoadableSect)(const BinFilePtr);
+typedef void* (*BinReadSect)(const BinFilePtr, const BinSectPtr);
+typedef uint64_t (*BinGetAmountSect)(const BinFilePtr);
+typedef const char* (*BinGetSectName)(const BinFilePtr, const BinSectPtr);
+typedef uint64_t (*BinGetSectSize)(const BinSectPtr);
+typedef uint64_t (*BinGetSectVaddr)(const BinSectPtr);
+typedef uint64_t (*BinGetSectFileoff)(const BinSectPtr);
+typedef uint64_t (*BinGetRelocationForAddr)(const BinFilePtr, const BinSectPtr, uint64_t);
+typedef void* (*BinGetRelocationDataAddr)(const BinFilePtr, const char*);
 
 typedef struct {
-    BIN_FILE_TYPE type;
-    BinFile *binFile;
-    BinParse binParse;
-    BinFree binFree;
-    BinHook binHook;
+    BIN_FILE_TYPE            type;
+    BinFilePtr               bin;
+    BinParse                 parse;
+    BinFree                  free;
+    BinHook                  hook;
+    BinFullCheck             check;
+    BinPrintSymbol           printSymbol;
+    BinPrintSymbols          printSymbols;
+    BinGetSymByName          getSymByName;
+    BinGetSymName            getSymName;
+    BinCmpSym                cmpSym;
+    BinGetSSymTab            getSSymTab;
+    BinGetSSymSortTab        getSSymSortTab;
+    BinGetAmountSSym         getAmountSSym;
+    BinGetSSymAddr           getSSymAddr;
+    BinGetAddrSymByName      getAddrSymByName;
+    BinGetSSymSize           getSSymSize;
+    BinGetSSymFileoff        getSSymFileoff;
+    BinGetDSymIndex          getDSymIndex;
+    BinGetAmountSegment      getAmountSegment;
+    BinGetSectByName         getSectByName;
+    BinGetLastLoadableSect   getLastLoadableSect;
+    BinReadSect              readSect;
+    BinGetAmountSect         getAmountSect;
+    BinGetSectName           getSectName;
+    BinGetSectSize           getSectSize;
+    BinGetSectVaddr          getSectVaddr;
+    BinGetSectFileoff        getSectFileoff;
+    BinGetRelocationForAddr  getRelocationForAddr;
+    BinGetRelocationDataAddr getRelocationDataAddr;
 } BinParser;
 
 extern BinParser binParser;
