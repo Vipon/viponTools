a4127e06ccce1b76a99c22580e7b6774eba0aa09
Vipon <valerakonychev@gmail.com>
2021-06-30 12:38:01 +0300

    Create common include directory.
    * Move arch.h t ocommon include directory.
    * Add os.h - contains predefined macro for detecting OS.
    * Add elf headers for build elfParse on windows.

---
+++ cTools/include/arch.h
@@ -0,0 +1,53 @@
+/***
+ * MIT License
+ *
+ * Copyright (c) 2021 Konychev Valera
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __ARCH_H
+#define __ARCH_H
+
+#if defined(__GNUC__) || defined(__clang__)
+# if defined(i386) || defined(__i386) || defined(__i386__)
+#  define IA32_DEFINED 1
+# endif
+
+# if defined(__amd64) || defined(__amd64__) || defined(__x86_64) || defined(__x86_64__)
+#  define IA32_DEFINED 1
+#  define IA32E_DEFINED 1
+#  define AMD64_DEFINED 1
+#  define X86_64_DEFINED 1
+# endif
+
+# if defined(__aarch64__)
+#  define ARM64_DEFINED 1
+#  define ARMV8_DEFINED 1
+#  define AARCH64_DEFINED 1
+# endif
+
+# if defined(__arm__)
+#  define ARM32_DEFINED 1
+# endif
+#else
+# error Unknown compiler type
+#endif
+
+#endif /* __ARCH_H */
---
+++ cTools/include/elf.h
@@ -0,0 +1,7 @@
+#ifndef __ELF_H
+#define __ELF_H
+
+#include "sys/elf32.h"
+#include "sys/elf64.h"
+
+#endif /* __ELF_H */
