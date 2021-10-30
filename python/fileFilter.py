d80295121bbba46982e655312b1187936dbfdddf
Vipon <valerakonychev@gmail.com>
2021-10-30 04:34:35 +0300

    Add licence to  files.

--- python/fileFilter.py
+++ python/fileFilter.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2020 Konychev Valera
+# Copyright (c) 2020-201 Konychev Valera
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
