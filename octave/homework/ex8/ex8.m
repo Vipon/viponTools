2035653d4c2452a5e9cb27a4095fc49b79b01acc
Vipon <valerakonychev@gmail.com>
2022-03-21 02:09:50 +0300

    Add solution to the 8th Home Work.

--- octave/homework/ex8/ex8.m
+++ octave/homework/ex8/ex8.m
@@ -46,9 +46,9 @@ pause
 %% ================== Part 2: Estimate the dataset statistics ===================
 %  For this exercise, we assume a Gaussian distribution for the dataset.
 %
-%  We first estimate the parameters of our assumed Gaussian distribution, 
-%  then compute the probabilities for each of the points and then visualize 
-%  both the overall distribution and where each of the points falls in 
+%  We first estimate the parameters of our assumed Gaussian distribution,
+%  then compute the probabilities for each of the points and then visualize
+%  both the overall distribution and where each of the points falls in
 %  terms of that distribution.
 %
 fprintf('Visualizing Gaussian fit.\n\n');
@@ -56,7 +56,7 @@ fprintf('Visualizing Gaussian fit.\n\n');
 %  Estimate my and sigma2
 [mu sigma2] = estimateGaussian(X);
 
-%  Returns the density of the multivariate normal at each data point (row) 
+%  Returns the density of the multivariate normal at each data point (row)
 %  of X
 p = multivariateGaussian(X, mu, sigma2);
 
@@ -71,7 +71,7 @@ pause;
 %% ================== Part 3: Find Outliers ===================
 %  Now you will find a good epsilon threshold using a cross-validation set
 %  probabilities given the estimated Gaussian distribution
-% 
+%
 
 pval = multivariateGaussian(Xval, mu, sigma2);
 
@@ -93,8 +93,8 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ================== Part 4: Multidimensional Outliers ===================
-%  We will now use the code from the previous part and apply it to a 
-%  harder problem in which more features describe each datapoint and only 
+%  We will now use the code from the previous part and apply it to a
+%  harder problem in which more features describe each datapoint and only
 %  some features indicate whether a point is an outlier.
 %
 
@@ -105,7 +105,7 @@ load('ex8data2.mat');
 %  Apply the same steps to the larger dataset
 [mu sigma2] = estimateGaussian(X);
 
-%  Training set 
+%  Training set
 p = multivariateGaussian(X, mu, sigma2);
 
 %  Cross-validation set
