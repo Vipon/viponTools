2bac463fff46bc5f32e0492f23510144f2751b3d
vipon <valerakonychev@gmail.com>
2023-06-23 19:28:45 +0200

    Improve arch defines.

--- cTools/include/arch.h
+++ cTools/include/arch.h
@@ -25,9 +25,26 @@
 #ifndef __ARCH_H
 #define __ARCH_H
 
+typedef enum {
+    UNKNOWN_ARCH = -1,
+    // x86 32-bit
+    IA32 = 0,
+    X86 = IA32,
+    // x86 64-bit
+    IA32E,
+    X86_64 = IA32E,
+    AMD64 = IA32E,
+    // arm 32-bit
+    ARM,
+    // arm 64-bit
+    ARM64,
+    AARCH64 = ARM64
+} Arch;
+
 #if defined(__GNUC__) || defined(__clang__)
 # if defined(i386) || defined(__i386) || defined(__i386__)
 #  define IA32_DEFINED 1
+#  define ARCH X86
 # endif
 
 # if defined(__amd64) || defined(__amd64__) || defined(__x86_64) || defined(__x86_64__)
@@ -35,16 +52,19 @@
 #  define IA32E_DEFINED 1
 #  define AMD64_DEFINED 1
 #  define X86_64_DEFINED 1
+#  define ARCH X86_64
 # endif
 
 # if defined(__aarch64__)
 #  define ARM64_DEFINED 1
 #  define ARMV8_DEFINED 1
 #  define AARCH64_DEFINED 1
+#  define ARCH AARCH64
 # endif
 
 # if defined(__arm__)
 #  define ARM32_DEFINED 1
+#  define ARCH ARM
 # endif
 #else
 # error Unknown compiler type
